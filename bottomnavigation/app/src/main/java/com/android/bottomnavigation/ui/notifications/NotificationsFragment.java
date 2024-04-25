4780bd5a23bf8cd47dd7be3ee4962f1ced1250b1
Vardhannn <iamsrivardhann@gmail.com>
2024-04-26 00:30:27 +0530

    updating

--- bottomnavigation/app/src/main/java/com/android/bottomnavigation/ui/notifications/NotificationsFragment.java
+++ bottomnavigation/app/src/main/java/com/android/bottomnavigation/ui/notifications/NotificationsFragment.java
@@ -1,44 +1,62 @@
 package com.android.bottomnavigation.ui.notifications;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.view.Window;
+import android.widget.Button;
 
 import com.android.bottomnavigation.R;
 import com.android.bottomnavigation.login;
 import com.google.firebase.auth.FirebaseAuth;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.fragment.app.DialogFragment;
 
-import com.android.bottomnavigation.databinding.FragmentNotificationsBinding;
 
-public class NotificationsFragment extends Fragment {
+public class NotificationsFragment extends DialogFragment {
 
     private NotificationsFragment binding;
-
     private FirebaseAuth mAuth;
-
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
     }
 
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE); // Remove title bar
+        return dialog;
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notifications, container, false);
-    }
+        View rootView = inflater.inflate(R.layout.custom_dialog_box, container, false);
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        logout();
+        Button logoutButton = rootView.findViewById(R.id.button2);
+        Button cancelButton = rootView.findViewById(R.id.button);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        return rootView;
     }
 
     private void logout() {
@@ -47,6 +65,11 @@ public class NotificationsFragment extends Fragment {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
         getActivity().finish();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 
     @Override
